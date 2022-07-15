d2b03550db918130136b301d5dfc659377ff4ec7
kpvproctor <108880933+kpvproctor@users.noreply.github.com>
2022-07-15 11:51:14 -0500

    Added logic for z-index manipulation

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let pageElements = getComputedStyle(rootElements);
 // Grabs specific variables in the stylesheet for us to use
 let startingSize = Number(pageElements.getPropertyValue('--starting-size').replace('px', ''));
 let circleCount = Number(pageElements.getPropertyValue('--circle-count'));
+let startingZIndex = circleCount;
 let circleGap = Number(pageElements.getPropertyValue('--gap').replace('px', ''));
 
 // Selects the 'circle-container' div from the DOM
@@ -19,14 +20,16 @@ for (let i = 1; i <= circleCount; i++) {
         let currentCircle = document.getElementById("circle-" + JSON.stringify(i));
         let currentCircleHeight = startingSize;
         let currentCircleWidth = startingSize;
-        increaseSize(currentCircle, currentCircleHeight, currentCircleWidth);
+        let currentCircleZIndex = startingZIndex;
+        increaseSize(currentCircle, currentCircleHeight, currentCircleWidth, currentCircleZIndex);
     } else {
         circleContainer.appendChild(createCircleElement(i));
         let currentCircle = document.getElementById("circle-" + JSON.stringify(i));
         let previousCircle = document.getElementById("circle-" + (Number(JSON.stringify(i)) - 1));
         let previousCircleHeight = previousCircle.offsetHeight;
         let previousCircleWidth = previousCircle.offsetWidth;
-        increaseSize(currentCircle, previousCircleHeight, previousCircleWidth);
+        let previousCircleZIndex = Number(previousCircle.style.zIndex);
+        increaseSize(currentCircle, previousCircleHeight, previousCircleWidth, previousCircleZIndex);
     }
   }
 
@@ -37,9 +40,15 @@ function createCircleElement(circleNum) {
     return circleElement;
 }
 
-function increaseSize(elementId, currentHeight, currentWidth) {
-    newHeight = currentHeight + circleGap
-    newWidth = currentWidth + circleGap
-    elementId.style.height = `${newHeight}px`
-    elementId.style.width = `${newWidth}px`
+function increaseSize(elementId, currentHeight, currentWidth, currentZIndex) {
+    newHeight = currentHeight + circleGap;
+    newWidth = currentWidth + circleGap;
+    newZIndex = currentZIndex -= 1;
+    elementId.style.height = `${newHeight}px`;
+    elementId.style.width = `${newWidth}px`;
+    elementId.style.zIndex = `${newZIndex}`;
 }
+
+function zIndexFunction(elementId) {
+    alert(document.getElementById(elementId).style.zIndex);
+    }
